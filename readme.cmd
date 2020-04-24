0bc183157dfb478d26c5d638284e673b69722490
jffernique <45580992+jffernique@users.noreply.github.com>
2020-04-24 20:59:46 +0200

    Create readme.cmd

---
+++ readme.cmd
@@ -0,0 +1,9 @@
+Python for propose : team +0.5 and matchs +1.5
+
+It s an example for scrap and study result. save your money dont bet with my prog.
+
+but if you win one billion euros ---->
+
+Bitcoin adress for donation 3PfE8ehRtHqGfFfujTEHjaHpTxhaNbQMbz
+
+thanks

43ab152679bd8d1541aca0d1174dd91b1733f0b9
jffernique <45580992+jffernique@users.noreply.github.com>
2021-09-09 22:33:25 +0200

    Update readme.cmd

--- readme.cmd
+++ readme.cmd
@@ -4,6 +4,6 @@ It s an example for scrap and study result. save your money dont bet with my pro
 
 but if you win one billion euros ---->
 
-Bitcoin adress for donation 3PfE8ehRtHqGfFfujTEHjaHpTxhaNbQMbz
+Bitcoin adress for donation bc1qnmnpc20pwch6j67k4v493pgkax3hnjryfmtrty
 
 thanks

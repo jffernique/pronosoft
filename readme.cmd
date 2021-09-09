6540282819d18ea150b8bae5837cbc1495606e93
jffernique <45580992+jffernique@users.noreply.github.com>
2021-09-09 22:34:00 +0200

    Update readme.cmd

--- readme.cmd
+++ readme.cmd
@@ -2,7 +2,7 @@ Python for propose : team +0.5 and matchs +1.5
 
 It s an example for scrap and study result. save your money dont bet with my prog.
 
-but if you win one billion euros ---->
+but if you win one billion $£€ ---->
 
 Bitcoin adress for donation bc1qnmnpc20pwch6j67k4v493pgkax3hnjryfmtrty
 
